a3852eb967915c84e0996e36ca5389af6efd3c8b
Candice Canoso <mail@cndce.me>
2021-08-10 04:55:05 +0800

    Transfer from experiments

---
+++ assets/themes/boxxerworld/cndce-container.php
@@ -0,0 +1,18 @@
+<?php
+    include('config.php');
+
+?>
+
+
+<div id="cndce-gloves-container" class="cndce-container" data-active-editable="color">
+
+
+
+    
+    <?php include 'components/controls-top.php' ?>
+
+    <?php include 'components/controls-summary.php' ?>
+
+    <?php include 'components/controls-panel.php' ?>
+
+</div>
